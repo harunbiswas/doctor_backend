3cd2fb5f3b9b42e1a46d2aa257b838179f802530
Harun <harun@harun@gmail.com>
2022-09-29 00:55:49 +0600

    final update subscription

--- controller/frontend/subscriptionController.js
+++ controller/frontend/subscriptionController.js
@@ -17,31 +17,25 @@ async function createSubscription(req, res, next) {
         req.body.time === "month"
           ? new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
           : (isLeapyear(date.getFullYear()) && 366) || 365;
-      let expireDate =
-        req.body.time === "month"
-          ? new Date(date.getFullYear(), date.getMonth() + 1, date.getDate())
-          : new Date(date.getFullYear() + 1, date.getMonth());
+
       const token = jwt.sign(req.body, process.env.JWT_SECTET, {
         expiresIn: 86400 * time,
       });
 
       const decode = jwt.verify(token, process.env.JWT_SECTET);
-      const ex = new Date(decode.exp).getFullYear();
-      console.log(ex, decode.exp);
+      let expireDate = new Date(decode.exp * 1000);
 
       expireDate =
         expireDate.getFullYear() +
         ":" +
-        expireDate.getMonth() +
+        (expireDate.getMonth() + 1) +
         ":" +
-        expireDate.getDate() +
-        1;
+        expireDate.getDate();
 
       con.query(
         `SELECT * FROM patients WHERE userId = ${req.user.id}`,
         (err, rows) => {
           if (err) {
-            console.log(err);
             res.status(500).json("Internal server errors");
           } else {
             if (rows.length > 0) {
@@ -100,68 +94,61 @@ async function updateSubscription(req, res, next) {
                 `SELECT * FROM subscriptions WHERE patientId = ${rows[0].id}`,
                 (err, rows1) => {
                   if ((err, rows1.length > 0)) {
-                    const preExpireDate = rows1[0].expireDate;
-
                     const date = new Date();
-                    let time;
 
                     const decode = jwt.verify(
                       rows1[0].token,
                       process.env.JWT_SECTET
                     );
-                    const leftyear = rows1[0].expireDate.getFullYear();
-                    const leftmonth = rows1[0].expireDate.getMonth();
-                    const leftDate = rows1[0].expireDate.getDate();
-                    const currentyear = date.getFullYear();
-                    const currentmonth = date.getMonth();
-                    const currentDate = date.getDate();
+                    const currentTime = date.getTime() / 1000;
+                    const expireTime = rows1[0].expireDate.getTime() / 1000;
+                    const bTime = expireTime - currentTime;
 
-                    console.log(
-                      leftyear,
-                      leftmonth,
-                      leftDate,
-                      currentyear,
-                      currentmonth,
-                      currentDate,
-                      rows1[0].expireDate
-                    );
-                    if (decode) {
-                      time =
-                        req.body.time === "month"
-                          ? new Date(
-                              date.getFullYear(),
-                              date.getMonth() + 1,
-                              0
-                            ).getDate()
-                          : (isLeapyear(date.getFullYear()) && 366) || 365;
-                    } else {
-                      time =
-                        req.body.time === "month"
-                          ? new Date(
-                              date.getFullYear(),
-                              date.getMonth() + 1,
-                              0
-                            ).getDate()
-                          : (isLeapyear(date.getFullYear()) && 366) || 365;
-                    }
+                    let time =
+                      req.body.time === "month"
+                        ? new Date(
+                            rows1[0].expireDate.getFullYear(),
+                            rows1[0].expireDate.getMonth() + 1,
+                            0
+                          ).getDate()
+                        : (isLeapyear(rows1[0].expireDate.getFullYear()) &&
+                            366) ||
+                          365;
 
-                    // const sql = `INSERT INTO subscriptions(patientId, token, expireDate) VALUES(${
-                    //   rows[0].id
-                    // }, ${JSON.stringify(token)}, ${JSON.stringify(
-                    //   expireDate
-                    // )})`;
-                    // con.query(sql, (err) => {
-                    //   if (err) {
-                    //     res.status(500).json("Internal server errors!");
-                    //   } else {
-                    //     res.status(200).json({
-                    //       data: {
-                    //         token,
-                    //         expireDate,
-                    //       },
-                    //     });
-                    //   }
-                    // });
+                    time = parseInt(86400 * time + (bTime > 0 ? bTime : 0));
+
+                    const token = jwt.sign(req.body, process.env.JWT_SECTET, {
+                      expiresIn: time,
+                    });
+
+                    const decodeEx = jwt.verify(token, process.env.JWT_SECTET);
+                    let expireDate = new Date(decodeEx.exp * 1000);
+
+                    expireDate =
+                      expireDate.getFullYear() +
+                      ":" +
+                      (expireDate.getMonth() + 1) +
+                      ":" +
+                      expireDate.getDate();
+
+                    const sql = `UPDATE subscriptions SET  token=${JSON.stringify(
+                      token
+                    )}, expireDate=${JSON.stringify(expireDate)} WHERE id=${
+                      rows1[0].id
+                    }`;
+                    con.query(sql, (err) => {
+                      if (err) {
+                        console.log(err);
+                        res.status(500).json("Internal server errors!");
+                      } else {
+                        res.status(200).json({
+                          data: {
+                            token,
+                            expireDate,
+                          },
+                        });
+                      }
+                    });
                   } else {
                     res.status(200).json("You haven't any package!");
                   }
